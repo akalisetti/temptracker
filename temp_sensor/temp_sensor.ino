da9d6d2d635ffba39a953ccc7a7639678a3028c3
Aarya <kalaar216@gmail.com>
2021-03-09 00:01:07 -0800

    Sending to github

--- temp_sensor/temp_sensor.ino
+++ temp_sensor/temp_sensor.ino
@@ -7,10 +7,10 @@ void setup() {
   Serial.begin(9600);
   Wire.begin();
 
-  //scan for temperature in every 20 sec untill a sensor is found. Scan for both addresses 0x48 and 0x49
+  //scan for temperature in every 3 sec untill a sensor is found. Scan for both addresses 0x48 and 0x49
   while(!tempSensor.scanAvailableSensors()){
     Serial.println("Couldn't find the temperature sensor, please connect the sensor." );
-    delay(30000);
+    delay(3000);  
   }
 
   tempSensor.begin();   // set continuos mode, active mode
@@ -18,8 +18,7 @@ void setup() {
 
 void loop() {
 
-  float temp = tempSensor.getTemperature(); // read temperature for every 100ms
-  Serial.print(temp ,2);
-  Serial.println("'c" );
+  float temp = tempSensor.getTemperature(); // read temperature for every 1000ms
+  Serial.println(temp);
   delay(5000);
 }
